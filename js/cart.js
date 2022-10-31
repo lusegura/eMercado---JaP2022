891ab2e57c9c4d607b9b054aefbd9b98c9e64d0e
lusegura <110472657+lusegura@users.noreply.github.com>
2022-10-31 20:03:50 -0300

    entrega 6
    js

--- js/cart.js
+++ js/cart.js
@@ -1,10 +1,9 @@
 
 let carrito = [];
 let ID= 25801;
-
 let CartInfoURL = CART_INFO_URL+`${ID}`+EXT_TYPE;
 
-
+//CARGA DE PAGINA:
 
 document.addEventListener("DOMContentLoaded", function(){
     getJSONData(CartInfoURL).then(result=>{
@@ -17,11 +16,18 @@ document.addEventListener("DOMContentLoaded", function(){
             console.log(typeof carrito.articles)
             subtotal();
             document.getElementById("cantidad").addEventListener("input", subtotal);
-           
+            document.getElementById("opcionEnvio").addEventListener("click", costoEnvioCostos)
+            subtotalCostos()
+            document.getElementById("cantidad").addEventListener("input", subtotalCostos); //al cambiar el valor aparece como undefined en la consola del navegador
+            document.getElementById("cantidad").addEventListener("input", costoEnvioCostos)
+            document.getElementById("formaPago").addEventListener("click", modoPago)
         } else{
             alert("error" + result.status);
         }
     });
+
+
+//FUNCION QUE MUESTRA PRODUCTO PRECARGADO EN CARRITO:
 
 function mostrarCarrito(carrito){
     let htmlContentToAppend = "";
@@ -33,23 +39,210 @@ function mostrarCarrito(carrito){
           <tr>
             <th scope="row"> <img src="${articulo.image}" alt="${articulo.name}" class="img-thumbnail imagen-carrito"></th>
             <td>${articulo.name}</td>
-            <td>${articulo.currency}<input type="number" id="costo" value="${articulo.unitCost}" disabled></td>
-            <td><input type="number" id="cantidad" value="${articulo.count}"></td>
-            <th id="moneda">${articulo.currency}<th id="subtotal"></th></th>
+            <td>${articulo.currency}<input type="number" id="costo" class="inputcosto" value="${articulo.unitCost}" disabled></td>
+            <td><input class="form-control" type="number" id="cantidad" name="cant" value="${articulo.count}"></td>
+            <th id="moneda">${articulo.currency}<th ><input type="number" id="subtotal" class="inputcosto" value="" disabled></input></th></th>
           </tr>
         </tbody>
             `
     }
 
         document.getElementById("carrito").innerHTML = htmlContentToAppend;
-        
+        document.getElementById("seleccionPago").innerHTML=`<p>No ha seleccionado</p>`
     };
 
+//SUBTOTAL DE TABLA:
+
  function subtotal() {
-    document.getElementById("subtotal").innerHTML= document.getElementById("cantidad").value * document.getElementById("costo").value; 
+    document.getElementById("subtotal").value= document.getElementById("cantidad").value * document.getElementById("costo").value;
 }
 
 
+//SUBTOTAL DE LOS COSTOS:
+
+function subtotalCostos(){ 
+document.getElementById("Subtotal1").innerHTML=` USD  ${document.getElementById("cantidad").value * document.getElementById("costo").value}`
+};
+
+//PORCENTAJE ENVIO Y TOTAL DE LOS COSTOS:
+
+function costoEnvioCostos(){
+        var radios = document.getElementsByName("envio");
+        for (let radio of radios)
+        {
+            if (radio.checked) {
+                let porcentaje = radio.value;
+                
+                document.getElementById("Subtotal2").innerHTML=`USD ${porcentaje * (document.getElementById("cantidad").value * document.getElementById("costo").value)/100}`;//subtotalCosto en lugar de costo
+                document.getElementById("Subtotal3").innerHTML=`USD ${porcentaje * (document.getElementById("cantidad").value * document.getElementById("costo").value)/100 + document.getElementById("cantidad").value * document.getElementById("costo").value}`;
+              }
+        }
+};
+
+
+// FUNCION DE MODO DE PAGO:
+
+function modoPago(){
+    var radioss1 = document.getElementsByName("pago");
+    for (let radio of radioss1) {
+        if (radio.checked) {
+           let MetodoPago =radio.value;
+           if(MetodoPago === "Transferencia"){
+            cred1.disabled = true;
+            cred2.disabled = true;
+            cred3.disabled = true;
+            cred1.value = " ";
+            cred2.value = " ";
+            cred3.value = " ";
+            trans.disabled = false;
+            document.getElementById("seleccionPago").innerHTML=`<p>Transferencia bancaria</p>`
+           }else{
+            trans.disabled = true; 
+            trans.value = " ";
+            cred1.disabled = false;
+            cred2.disabled = false;
+            cred3.disabled = false;
+            document.getElementById("seleccionPago").innerHTML=`<p>Tarjeta de Crédito</p>`
+           }
+
+        }
+    }
+};
+
 });
 
+// VALIDACIONES:
+
+let form1 = document.getElementById("form1");
+let calle1 = document.getElementById("validationCalle");
+let numero1 = document.getElementById("validationNumero");
+let esquina1 = document.getElementById("validationEsquina");
+let cantidadArt = document.getElementById("cantidad");
+let formaDePago = document.getElementById("seleccionPago");
+
+
+// CARGA Y EJECUCION DE VALIDACIONES:
+document.addEventListener("DOMContentLoaded", function () {
+  mostrarValidacion();
+  form1.addEventListener("submit", validacionTotal);
+ 
+}); 
+
+//modificar class en tiempo real: 
+
+function mostrarValidacion(){
+  for (const input of form1) {
+    input.addEventListener("change", (event) => {
+      if (input.value !== "") {  
+        input.classList.remove("is-invalid");
+        input.classList.add("is-valid");
+      }else{
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+      };   
+    });
+  }
+ }
+
+
+//validacion de todo:
+
+function validacionTotal(evento){
+   if(validarFormulario(evento) && validarFormulario1(evento) && validarFormulario2(evento) && validarFormulario3(evento) && validarFormulario4(evento)){
+        this.submit();  
+        alert("Su compra fue realizada de forma exitosa") 
+}else{
+  alert("Llene todos los datos para realizar su compra de forma exitosa")
+}};
+
+//validacion input cantidad:
+
+function validarFormulario2(evento) { 
+    evento.preventDefault();
+    let cant = document.getElementById("cantidad")
+     if(cant.value > 0){   
+      cant.classList.add("is-valid"); 
+        return true
+     }else{
+      cant.classList.add("is-invalid"); 
+        alert("input cantidad debera ser al menos 1")
+        return false
+}}
+
+//validacion radiobutton, tipo de envio:
+
+    function validarFormulario1(evento) { 
+      evento.preventDefault();
+       if(form1.envio[0].checked == true){      
+        return true
+       }else if(form1.envio[1].checked == true){
+        return true
+      }else if(form1.envio[2].checked == true){
+          return true
+        }else{
+          alert("debe seleccionar una forma de envio")
+        return false
+       }}
+
+// validacion datos direccion de envio:
+
+function validarFormulario(evento) { 
+    evento.preventDefault();
+    let calle = document.getElementById("validationCalle").value;
+    if(calle.length == 0) {
+      calle1.classList.add("is-invalid"); 
+    }else{
+        calle1.classList.remove("is-invalid");
+        calle1.classList.add("is-valid");
+    }
+    let numero = document.getElementById("validationNumero").value;
+    if (numero.length  == 0) {
+        numero1.classList.add("is-invalid");
+    }else{
+      numero1.classList.remove("is-invalid");
+      numero1.classList.add("is-valid");
+    }
+    let esquina = document.getElementById("validationEsquina").value;
+    if(esquina.length == 0) {
+        esquina1.classList.add("is-invalid");
+     }else{
+      esquina1.classList.remove("is-invalid");
+      esquina1.classList.add("is-valid");
+     }   
+return true
+}
+
+//validacion seleccion forma de pago:
+
+function validarFormulario3(evento){
+   if (document.getElementById("validationFormCheck1").checked){
+          return true;
+        }else if (document.getElementById("validationFormCheck2").checked){
+          return true;
+          }else{
+    alert("Debe elegir un metodo de pago")
+    return false;
+   }
+}
+
+//validacion Campos de forma de pago:
+
+function validarFormulario4(evento){
+  if (document.getElementById("validationFormCheck1").checked){
+    if( document.getElementById("cred1").value !== "" && document.getElementById("cred2").value !== "" && document.getElementById("cred3").value !== ""){  
+    return true;
+  }else{
+     alert("Debe completar todos los datos de la tarjeta")
+     return false;
+  }
+}else if(document.getElementById("validationFormCheck2").checked){ 
+    if(document.getElementById("trans").value !==""){     
+    return true;
+   }else{
+    alert("Debe completar los datos de la cuenta bancaria"); 
+   }}else{
+    alert("Debe completar los datos de pago")
+    return false;
+   }
+}
 
--- js/product-info.js
+++ js/product-info.js
@@ -1,10 +1,19 @@
 let producto=[];
 let comment=[];
 
+
 let ID= localStorage.getItem("ProductID");
 let ProductInfoURL = PRODUCT_INFO_URL+`${ID}`+EXT_TYPE;
 let ProductCommentsURL = PRODUCT_INFO_COMMENTS_URL+`${ID}`+EXT_TYPE;
 
+class Producto{
+    constructor(imagen, nombre, moneda, precio){
+        this._imagen = imagen,
+        this._nombre = nombre,
+        this._moneda = moneda,
+        this._precio = precio
+    }
+};
 
 document.addEventListener("DOMContentLoaded", function(){
     getJSONData(ProductInfoURL).then(result=>{
@@ -12,17 +21,25 @@ document.addEventListener("DOMContentLoaded", function(){
             producto = result.data;
             mostrarInfoProducto(producto);
             console.log(producto);
+            document.getElementById("agregarCarrito").addEventListener("click",function(){
+               /*  agregarACarrito();  ESTA FUNCION AGREGA AL LOCAL STORAGE SOLO LOS ID*/
+                agregarACarrito1(); //ESTA FUNCION INTENTA AGREGAR AL LOCAL STORAGE EL OBJETO
+               });
         } else{
             alert("error" + result.status);
         }
     })
 
+    
+    
 
 function mostrarInfoProducto(id){
 htmlContentToAppend="";
 htmlContentToAppend += `
-<div class="container" id="infoProducto">
-<h3 class="nombreProducto"> ${id.name} </h3><hr>
+
+<div class="container" id="infoProducto"> 
+<h3 class="nombreProducto"> ${id.name} </h3>
+<button class="btn btn-success" type="submit" id="agregarCarrito">Comprar!</button><hr>
 <strong class="subtitulo">Precio:</strong>
 <p> ${id.currency}  ${id.cost} </p>
 <strong class="subtitulo">Descripción:</strong>
@@ -47,7 +64,6 @@ htmlContentToAppend += `
               </button>
             </div>
 </div>
-
 <strong class="subtitulo"> Productos relacionados:</strong><br><br>
 </div>
 <div class="container">
@@ -55,12 +71,15 @@ htmlContentToAppend += `
 ${otrosProductos(id.relatedProducts)} 
 </div>
 </div>
-<strong class="subtitulo"> Comentarios:</strong><br><br
+<strong class="subtitulo"> Comentarios:</strong><br><br>
 `
 ;
+let agregarProducto = new Producto(id.images[0], id.name, id.currency, id.cost) // OBJETO A AGREGAR
+console.log(agregarProducto);// aca esta hecho el objeto y se visualiza en la consola
 
 document.getElementById("infoProducto").innerHTML= htmlContentToAppend
-};
+}
+;
 
 function mostrarImagenes(id){
     imagenesToAppend = ""
@@ -146,3 +165,106 @@ function setRelatedID(ProductID) {
 };
 //"REDIRECCION" = REESCRIBE EL ID DE PRODUCTO PARA QUE SE CAMBIE EL VALOR DEL ID PARA PODER CARGAR 
 //LOS DATOS DEL NUEVO PRODUCTO.LINEAS 143 AL 145 ENTREGA 4 
+
+
+/*  CARRITO SIN CONSIDERAR REPETICION DE PRODUCTO:
+
+    let mis_items=[]
+    let items= ID;
+ function agregarACarrito(){
+    if (localStorage.getItem("items")){
+        alert("hay")
+        items_json= localStorage.getItem("items"); // obtuve valor del localStorage (se llama items_json)
+        mis_json= JSON.parse(items_json); // lo converti a string (pasa a llamarse mis_json)
+        console.log(mis_json); 
+        mis_json.push(items); //le agregue a lo que ya tenia (mis_json) el ID del nuevo producto 
+        miCarrito = JSON.stringify(mis_json); // lo transformo en json de nuevo
+        localStorage.setItem("items", miCarrito);// lo subo al localStorage
+ }else{
+    alert("no hay")
+       mis_items.push(items); // al carrito(mis_items) vacio se le agrega el producto(items)
+       let items_json = JSON.stringify(mis_items); // convertimos el carito a json (pasa a llamarse items_json)
+       localStorage.setItem("items", items_json); //subimos al localstorage
+ }
+}; */
+
+
+/* CARRITO CONSIDERANDO REPETICION DE PRODUCTO:
+
+let mis_items=[]
+let items= ID;
+function agregarACarrito(){
+if (localStorage.getItem("items")){
+    alert("hay")
+    items_json= localStorage.getItem("items"); // obtuve valor del localStorage (se llama items_json)
+    mis_json= JSON.parse(items_json); // lo converti a string (pasa a llamarse mis_json)
+    if(mis_json.indexOf(items) != -1){ // si se encuentra el elemento en el arreglo, no se modifica 
+        alert("ya esta")
+        miCarrito = JSON.stringify(mis_json); // lo transformo en json de nuevo
+        localStorage.setItem("items", miCarrito);// lo subo al localStorage
+    }else{ // si no se encuentra en el arreglo (el indexOf da -1, se agrega al arreglo el elemento)
+    console.log(mis_json); 
+    mis_json.push(items); //le agregue a lo que ya tenia (mis_json) el ID del nuevo producto 
+    miCarrito = JSON.stringify(mis_json); // lo transformo en json de nuevo
+    localStorage.setItem("items", miCarrito);// lo subo al localStorage
+}}else{
+alert("no hay")
+   mis_items.push(items); // al carrito(mis_items) vacio se le agrega el producto(items)
+   let items_json = JSON.stringify(mis_items); // convertimos el carito a json (pasa a llamarse items_json)
+   localStorage.setItem("items", items_json); //subimos al localstorage
+}
+}; */
+
+
+
+
+/* GUARDA ID EN LOCAL STORAGE EN VEZ DEL OBJETO
+
+let mi_Carrito=[]
+let agregar= ID;
+function agregarACarrito(){
+if (localStorage.getItem("Carrito")){
+    alert("hay productos en carrito")
+    agregar_json= localStorage.getItem("Carrito"); 
+    a_agregar= JSON.parse(agregar_json); 
+    if(a_agregar.indexOf(agregar) != -1){ 
+        alert("ya esta")
+        miCarrito = JSON.stringify(a_agregar); 
+        localStorage.setItem("Carrito", miCarrito);
+    }else{ 
+    console.log(a_agregar); 
+    a_agregar.push(agregar); 
+    miCarrito = JSON.stringify(a_agregar); 
+    localStorage.setItem("Carrito", miCarrito);
+}}else{
+alert("no hay productos en carrito")
+mi_Carrito.push(agregar); 
+   let agregar_json = JSON.stringify(mi_Carrito); 
+   localStorage.setItem("Carrito", agregar_json); 
+}
+}; */
+
+//INTENTO DE GUARDAR EL OBJETO agregarProducto EN EL LOCAL STORAGE:
+let mi_Carrito=[]
+function agregarACarrito1(){
+    if (localStorage.getItem("Carrito")){
+        alert("hay productos en carrito")
+        agregar_json= localStorage.getItem("Carrito"); 
+        a_agregar= JSON.parse(agregar_json); 
+        if(a_agregar.indexOf(agregarProducto) != -1){ 
+            alert("ya esta")
+            mi_Carrito = JSON.stringify(a_agregar); 
+            localStorage.setItem("Carrito", mi_Carrito);
+        }else{ 
+        console.log(a_agregar); 
+        a_agregar.push(agregarProducto); 
+        mi_Carrito = JSON.stringify(a_agregar); 
+        localStorage.setItem("Carrito", mi_Carrito);
+    }}else{
+    alert("no hay productos en carrito")
+    mi_Carrito.push(agregarProducto); 
+       let agregar_json = JSON.stringify(mi_Carrito); 
+       localStorage.setItem("Carrito", agregar_json); 
+    }
+    };
+    
